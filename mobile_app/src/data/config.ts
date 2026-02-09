14253fdcf904c4624dfacd6a2a63310df9ded661
keiry0902 <keirym0902@gmail.com>
2026-02-09 00:15:39 -0500

    Cambio owner to keirym9

--- mobile_app/src/data/config.ts
+++ mobile_app/src/data/config.ts
@@ -6,7 +6,7 @@ type ExtraConfig = {
 };
 
 const DEFAULT_ANDROID_BASE_URL = "http://10.0.2.2:8000" //Diego http://192.168.20.75:8000";
-const DEFAULT_IOS_BASE_URL = "http://127.0.0.1:8000";
+const DEFAULT_IOS_BASE_URL = "http://192.168.1.19:8000";
 
 const getEnvBaseUrl = (): string | undefined => {
   try {
